484aa7b25e1cf6db2520d092c30efd186252c13b
KateZin <kate.zinyakova@gmail.com>
2021-12-05 11:33:50 +0300

    add functional tests with correct input data

--- 3-Zinyakova-D20/Subset-Test-D20/test.cpp
+++ 3-Zinyakova-D20/Subset-Test-D20/test.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <Windows.h>
 
 /*
 IDE: Visual Studio 2019 Community Edition
@@ -13,19 +14,19 @@ PC configuration:
 	RAM:            8.00 GB
 	HDD:            256 GB */
 
-//   Time: 4.292922 ms
-//   Time with reading from file: 11.554175 ms
+//   Time: 5.515 ms
+//   Time with reading from file: 26.811 ms
 
-const int count = 1000000;
-#define MAX_LENGTH 1000000
+const int count = 999;
 
 TEST(StressTest, SubsetSum) {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
 	_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
 
-	FILE* fstdin = fopen("C:/Users/z.kate/Desktop/Data.txt", "w");
-	char* name = "C:/Users/z.kate/Desktop/checkData.txt";
+	char* input = "C:/Users/z.kate/Desktop/3 сем/структуры и алгосы/for git/alg-dstruct/3-Zinyakova-D20/Subset-Test-D20/test_data/StressTest/Data.txt";
+	char* name = "C:/Users/z.kate/Desktop/3 сем/структуры и алгосы/for git/alg-dstruct/3-Zinyakova-D20/Subset-Test-D20/test_data/StressTest/checkData.txt";
+	FILE* fstdin = fopen(input, "w");
 	srand(time(NULL));
 	if (fstdin == NULL) {
 		printf("Error in file");
@@ -47,83 +48,28 @@ TEST(StressTest, SubsetSum) {
 	fclose(fstdin);
 	free(countBuf);
 	// read file and run functions
-	fstdin = fopen("C:/Users/z.kate/Desktop/Data.txt", "r");
-	if (fstdin == NULL) {
-		printf("Error in file");
-		ASSERT_TRUE(fstdin);
-	}
-	countBuf = (int*)malloc(MAX_LENGTH * sizeof(int));
-	if (countBuf == NULL) {
-		printf("Error in malloc");
-		fclose(fstdin);
-		ASSERT_TRUE(countBuf);
-	}
-	char* check = fgets((char*)countBuf, MAX_LENGTH, fstdin);
-	if (check == NULL) {
-		fclose(fstdin);
-		free(countBuf);
+	int* set = NULL;
+	int B, N, checkSum = 0;
+	int check = ReadData(input, &B, &N, &set);
+	if (check == -1) {
+		printf("error in reading data");
 		ASSERT_TRUE(check);
-	}
-	int num = atoi((char*)countBuf);
-	check = fgets((char*)countBuf, MAX_LENGTH, fstdin);
-	if (check == NULL) {
-		fclose(fstdin);
-		free(countBuf);
-		ASSERT_TRUE(check);
-	}
-	int n = atoi((char*)countBuf);
-	free(countBuf);
-	int* mass = ReadMass(fstdin, n);
-	if (mass == NULL) {
-		ASSERT_TRUE(mass);
 	}
 	int* res = NULL;
-	int size = SubsetSum(mass, n, num, &res);
+	int size = SubsetSum(set, N, B, &res);
 	if (size == -1) {
 		ASSERT_TRUE(size);
 	}
-	int toCheck = MassInFile(name, res, size);
-	if (toCheck = -1) {
-		ASSERT_TRUE(toCheck);
+	check = MassInFile(name, res, size);
+	if (check == -1) {
+		ASSERT_TRUE(check);
 	}
 	free(res);
+	free(set);
 	fclose(fstdin);
-	free(mass);
 }
 
-TEST(SubSetSum, CheckResInFile) {
-	char* name = "output.txt";
-	FILE* fp = fopen("output.txt", "r");
-	int subset[5] = { 2, 3, 4, 5, 9 };
-	int n = 5;
-	int sum = 10;
-	int* res = NULL;
-	int size = SubsetSum(subset, n, sum, &res);
-	MassInFile(name, res, size);
-	char str[100];
-	fgets(str, 100, fp);
-	EXPECT_EQ(strcmp(str, "2 3 5 "),  0);
-	fclose(fp);
-	free(res);
-}
-
-TEST(SubSetSum, NoSubset) {
-	char* name = "output.txt";
-	FILE* fp = fopen("output.txt", "r");
-	int subset[5] = { 2, 3, 4, 5, 9 };
-	int n = 5;
-	int sum = 1;
-	int* res = NULL;
-	int size = SubsetSum(subset, n, sum, &res);
-	MassInFile(name, res, size);
-	char str[100];
-	fgets(str, 100, fp);
-	EXPECT_EQ(strcmp(str, "0 "), 0);
-	fclose(fp);
-	free(res);
-}
-
-TEST(SubSetSum, CheckSizeOfSubSet) {
+TEST(FunctionalTest, CheckSizeOfSubSet) {
 	int subset[5] = { 2, 3, 4, 5, 9 };
 	int n = 5;
 	int sum = 10;
@@ -131,4 +77,131 @@ TEST(SubSetSum, CheckSizeOfSubSet) {
 	int size = SubsetSum(subset, n, sum, &res);
 	EXPECT_EQ(size, 3);
 	free(res);
+}
+
+TEST(FunctionalTest, noElements) {
+	char* filename = "C:/Users/z.kate/Desktop/3 сем/структуры и алгосы/for git/alg-dstruct/3-Zinyakova-D20/Subset-Test-D20/test_data/noElements/noElementsInput.txt";
+	char* name = "C:/Users/z.kate/Desktop/3 сем/структуры и алгосы/for git/alg-dstruct/3-Zinyakova-D20/Subset-Test-D20/test_data/noElements/noElementsOutput.txt";
+	int* set = NULL;
+	int B, N;
+	int check = ReadData(filename, &B, &N, &set);
+	if (check == -1) {
+		printf("error in reading data");
+		ASSERT_TRUE(check);
+	}
+	EXPECT_EQ(N, 0);
+	EXPECT_EQ(B, 10);
+	int* res = NULL;
+	int size = SubsetSum(set, N, B, &res);
+	if (size == -1) {
+		ASSERT_TRUE(size);
+	}
+	check = MassInFile(name, res, size);
+	if (check == -1) {
+		ASSERT_TRUE(check);
+	}
+	FILE* output = fopen(name, "r");
+	if (output == NULL) {
+		printf("error in file");
+		ASSERT_TRUE(output);
+	}
+	char str[100];
+	fgets(str, 100, output);
+	EXPECT_EQ(strcmp(str, "0 "), 0);
+	fclose(output);
+	free(res);
+	free(set);
+}
+
+TEST(FunctionalTest, TooManyElements) {
+	// there are no real set A in file as input is wrong (N>1000). Expect error
+	char* filename = "C:/Users/z.kate/Desktop/3 сем/структуры и алгосы/for git/alg-dstruct/3-Zinyakova-D20/Subset-Test-D20/test_data/TooManyElements/TooManyElements.txt";
+	char* name = "C:/Users/z.kate/Desktop/3 сем/структуры и алгосы/for git/alg-dstruct/3-Zinyakova-D20/Subset-Test-D20/test_data/TooManyElements/TooManyElements.txt";
+	int* set = NULL;
+	int B, N;
+	int toCheck = ReadData(filename, &B, &N, &set);
+	if (toCheck == -1) {
+		printf("error in reading data");
+		ASSERT_TRUE(toCheck);
+	}
+	EXPECT_EQ(N, 1001);
+	EXPECT_EQ(B, 10);
+	int* res = NULL;
+	int size = SubsetSum(set, N, B, &res);
+	EXPECT_EQ(size, -1);
+	free(res);
+	free(set);
+}
+
+TEST(FunctionalTest, NegativeElement) {
+	// input is wrong as there is a negative number. Expect error
+	char* filename = "C:/Users/z.kate/Desktop/3 сем/структуры и алгосы/for git/alg-dstruct/3-Zinyakova-D20/Subset-Test-D20/test_data/NegativeElement/NegativeElement.txt";
+	int* set = NULL;
+	int B, N;
+	int toCheck = ReadData(filename, &B, &N, &set);
+	if (toCheck == -1) {
+		printf("error in reading data");
+		ASSERT_TRUE(toCheck);
+	}
+	EXPECT_EQ(N, 5);
+	EXPECT_EQ(B, 10);
+	int* res = NULL;
+	int size = SubsetSum(set, N, B, &res);
+	EXPECT_EQ(size, -1);
+	free(res);
+	free(set);
+}
+
+TEST(FunctionalTest,ResultExistCheck) {
+	char* filename = "C:/Users/z.kate/Desktop/3 сем/структуры и алгосы/for git/alg-dstruct/3-Zinyakova-D20/Subset-Test-D20/test_data/ResultExistCheck/Input.txt";
+	char* output = "C:/Users/z.kate/Desktop/3 сем/структуры и алгосы/for git/alg-dstruct/3-Zinyakova-D20/Subset-Test-D20/test_data/ResultExistCheck/Output.txt";
+	int* set = NULL;
+	int B, N, checkSum = 0;
+	int toCheck = ReadData(filename, &B, &N, &set);
+	if (toCheck == -1) {
+		printf("error in reading data");
+		ASSERT_TRUE(toCheck);
+	}
+	EXPECT_EQ(N, 10);
+	EXPECT_EQ(B, 20);
+	int* res = NULL;
+	int size = SubsetSum(set, N, B, &res);
+	for (int i = 0; i < size; i++) {
+		checkSum += res[i];
+	}
+	EXPECT_EQ(checkSum, B);
+	free(res);
+	free(set);
+}
+
+
+TEST(FunctionalTest, ResultNotExistCheck) {
+	char* filename = "C:/Users/z.kate/Desktop/3 сем/структуры и алгосы/for git/alg-dstruct/3-Zinyakova-D20/Subset-Test-D20/test_data/ResultNotExistCheck/Input.txt";
+	char* output = "C:/Users/z.kate/Desktop/3 сем/структуры и алгосы/for git/alg-dstruct/3-Zinyakova-D20/Subset-Test-D20/test_data/ResultNotExistCheck/Output.txt";
+	int* set = NULL;
+	int B, N, checkSum = 0;
+	int check = ReadData(filename, &B, &N, &set);
+	if (check == -1) {
+		printf("error in reading data");
+		ASSERT_TRUE(check);
+	}
+	EXPECT_EQ(N, 10);
+	EXPECT_EQ(B, 65);
+	int* res = NULL;
+	int size = SubsetSum(set, N, B, &res);
+	EXPECT_EQ(size, 1);
+	check = MassInFile(output, res, size);
+	if (check == -1) {
+		ASSERT_TRUE(check);
+	}
+	FILE* fp = fopen(output, "r");
+	if (fp == NULL) {
+		printf("error in file");
+		ASSERT_TRUE(fp);
+	}
+	char str[100];
+	fgets(str, 100, fp);
+	EXPECT_EQ(strcmp(str, "0 "), 0);
+	free(res);
+	free(set);
 }
