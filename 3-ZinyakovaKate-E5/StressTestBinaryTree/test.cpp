804fd884e1c8290b4ad784cac57b7885a25fd223
KateZin <kate.zinyakova@gmail.com>
2021-12-19 11:18:18 +0300

    test changes

--- 3-ZinyakovaKate-E5/StressTestBinaryTree/test.cpp
+++ 3-ZinyakovaKate-E5/StressTestBinaryTree/test.cpp
@@ -3,7 +3,16 @@
 
 TEST(FunctionalTest, OnlyRoot) {
 	Tree* tree = nullptr;
-	tree = Insert(tree, 12);
+	tree = Insert(tree, 111111111);
+	FillWidth(&tree);
+	EXPECT_EQ(tree->width, 11);
+	PrintTree(tree, 1);
+	DestroyTree(tree);
+}
+
+TEST(FunctionalTest, TreeWithZero) {
+	Tree* tree = nullptr;
+	tree = Insert(tree, 0);
 	FillWidth(&tree);
 	EXPECT_EQ(tree->width, 2);
 	DestroyTree(tree);
@@ -11,24 +20,20 @@ TEST(FunctionalTest, OnlyRoot) {
 
 TEST(FunctionalTest, CheckResult) {
 	Tree* tree = nullptr;
-	tree = Insert(tree, 12);
-	tree = Insert(tree, 5);
+	tree = Insert(tree, 51);
 	tree = Insert(tree, 20);
-	tree = Insert(tree, 3);
-	tree = Insert(tree, 6);
-	tree = Insert(tree, 16);
-	tree = Insert(tree, 22);
-	tree = Insert(tree, 31);
-	tree = Insert(tree, 30);
+	tree = Insert(tree, 1);
+	tree = Insert(tree, 43);
+	tree = Insert(tree, 66);
+	tree = Insert(tree, 60);
 	FillWidth(&tree);
-	EXPECT_EQ(tree->left->left->width, 1);
-	EXPECT_EQ(tree->left->right->width, 1);
-	EXPECT_EQ(tree->left->width, 3);
-	EXPECT_EQ(tree->right->left->width, 2);
-	EXPECT_EQ(tree->right->right->width, 6);
-	EXPECT_EQ(tree->right->right->right->width, 4);
-	EXPECT_EQ(tree->right->right->right->left->width, 2);
-	EXPECT_EQ(tree->width, 15);
+	EXPECT_EQ(tree->left->left->width, 2);
+	EXPECT_EQ(tree->left->right->width, 3);
+	EXPECT_EQ(tree->left->width, 8);
+	EXPECT_EQ(tree->right->left->width, 3);
+	EXPECT_EQ(tree->right->width, 6);
+	EXPECT_EQ(tree->width, 18);
+	PrintTree(tree, 1);
 	DestroyTree(tree);
 }
 
@@ -40,32 +45,24 @@ TEST(FunctionalTest, NegativeElements) {
 		tree = Insert(tree, -11);
 		tree = Insert(tree, -14);
 		FillWidth(&tree);
-		EXPECT_EQ(tree->left->width, 3);
-		EXPECT_EQ((tree->right)->right->width, 2);
-		EXPECT_EQ(tree->right->left->width, 3);
-		EXPECT_EQ(tree->right->width, 8);
-		EXPECT_EQ(tree->width, 14);
+		EXPECT_EQ(tree->left->width, 4);
+		EXPECT_EQ((tree->right)->right->width, 3);
+		EXPECT_EQ(tree->right->left->width, 4);
+		EXPECT_EQ(tree->right->width, 12);
+		EXPECT_EQ(tree->width, 21);
+		PrintTree(tree, 1);
 		DestroyTree(tree);
 }
 
-TEST(FunctionalTest, TreeWithZero) {
+TEST(FunctionalTest, ElementsWithOverDigitInTheRoot) {
 	Tree* tree = nullptr;
-	tree = Insert(tree, 0);
+	tree = Insert(tree, 99);
+	tree = Insert(tree, 11);
+	tree = Insert(tree, 413);
 	FillWidth(&tree);
-	EXPECT_EQ(tree->width, 1);
-	DestroyTree(tree);
-}
-
-TEST(FunctionalTest, PrintTree) {
-	Tree* tree = nullptr;
-	tree = Insert(tree, 3);
-	tree = Insert(tree, 2);
-	tree = Insert(tree, 9);
-	tree = Insert(tree, 5);
-	tree = Insert(tree, 7);
-	tree = Insert(tree, 4);
-	tree = Insert(tree, 1);
-	FillWidth(&tree);
-	PrintTree(tree);
+	EXPECT_EQ(tree->right->width, 4);
+	EXPECT_EQ(tree->left->width, 3);
+	EXPECT_EQ(tree->width, 11);
+	PrintTree(tree, 1);
 	DestroyTree(tree);
 }
