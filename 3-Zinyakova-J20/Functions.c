8ad5f36e80746dda0dc7c9f6740f813776a8a447
KateZin <90635745+KateZin@users.noreply.github.com>
2022-05-24 23:58:26 +0300

    checked braces

--- 3-Zinyakova-J20/Functions.c
+++ 3-Zinyakova-J20/Functions.c
@@ -1,12 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include"Header.h"
+#include "Header.h"
 
 unsigned int Hash(const char* s) {
     unsigned int h = 0, g;
     while (*s) {
         h = (h << 4) + *s++;
         g = h & 0xf0000000L;
-        if (g){
+        if (g) {
             h ^= g >> 24;
         }
         h &= ~g;
@@ -17,7 +17,7 @@ unsigned int Hash(const char* s) {
 int FindNode(Node** table, const char* key) {
     unsigned index = Hash(key);
     const Node* it = table[index];
-    while (it != NULL && strcmp(it->data, key) != 0){
+    while (it != NULL && strcmp(it->data, key) != 0) {
         it = it->next;
     }
     if (it != NULL) {
@@ -29,14 +29,14 @@ int FindNode(Node** table, const char* key) {
 }
 
 int AddNode(Node** table, char* key) {
-    if (!FindNode(table, key)){
+    if (!FindNode(table, key)) {
         unsigned int index = Hash(key);
         Node* newNode = (Node*)malloc(sizeof * newNode);
-        if (newNode == NULL){
+        if (newNode == NULL) {
             return 0;
         }
         newNode->data = (char*)malloc((strlen(key) + 1) * sizeof(char));
-        if (newNode->data == NULL){
+        if (newNode->data == NULL) {
             free(newNode);
             return 0;
         }
@@ -53,14 +53,14 @@ int DeleteNode(Node** table, char* key) {
         unsigned int index = Hash(key);
         Node* it = table[index];
         Node* prev = NULL;
-        while (strcmp(it->data, key) != 0){
+        while (strcmp(it->data, key) != 0) {
             prev = it;
             it = it->next;
         }
-        if (prev){
+        if (prev) {
             prev->next = it->next;
         }
-        else{
+        else {
             table[index] = it->next;
         }
         free(it->data);
@@ -70,11 +70,11 @@ int DeleteNode(Node** table, char* key) {
 }
 
 void DeleteChain(Node** first) {
-    if (!(*first)){
+    if (!(*first)) {
         return;
     }
     Node* prev = NULL;
-    while ((*first)->next){
+    while ((*first)->next) {
         prev = (*first);
         (*first) = (*first)->next;
         free(prev->data);
@@ -86,7 +86,7 @@ void DeleteChain(Node** first) {
 
 int DeleteTable(Node** table) {
     Node* it = NULL;
-    for (int i = 0; i < TABLE_SIZE; i++){
+    for (int i = 0; i < TABLE_SIZE; i++) {
         DeleteChain(&table[i]);
     }
     return 0;
@@ -97,9 +97,9 @@ int Solution() {
     Node* table[TABLE_SIZE] = { NULL };
     char buf[BUF_SIZE] = { 0 };
     char value[10000] = { 0 };
-    while (fgets(buf, BUF_SIZE, stdin)){
+    while (fgets(buf, BUF_SIZE, stdin)) {
         sscanf(buf, "%c %s", &command, &value);
-        switch (command){
+        switch (command) {
         case 'a':
             AddNode(table, value);
             break;
@@ -107,12 +107,10 @@ int Solution() {
             DeleteNode(table, value);
             break;
         case 'f':
-            if (FindNode(table, value))
-            {
+            if (FindNode(table, value)) {
                 printf("%s", "yes\n");
             }
-            else
-            {
+            else {
                 printf("%s", "no\n");
             }
             break;
--- 3-Zinyakova-J20/main.c
+++ 3-Zinyakova-J20/main.c
@@ -1,4 +1,4 @@
-#include"Header.h"
+#include "Header.h"
 #pragma warning(disable: 4996)
 
 int main(void) {
